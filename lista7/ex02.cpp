cb221a3c96861c10f0d731c28c27c295ab332ad9
Raquel Santos <80298086+raquelsantoss@users.noreply.github.com>
2021-08-15 20:27:41 -0300

    Update ex02.cpp

--- lista7/ex02.cpp
+++ lista7/ex02.cpp
@@ -1,6 +1,4 @@
-//Nome: Raquel dos Santos Rezende 
-//Matricula: UC21100930
-//Curso: Ciência da Computação
+
 #include<stdio.h>
 #include<locale.h>
 
@@ -10,16 +8,16 @@ int main(){
 	float reais[50];
 	int quantidadeAlunos;
 	
-	printf("Digite o número de alunos: ");
+	printf("Digite o nÃºmero de alunos: ");
 	scanf("%i", &quantidadeAlunos);
 	
 	if(quantidadeAlunos > 50 || quantidadeAlunos <= 0){
-		printf("Valor inválido!");
+		printf("Valor invÃ¡lido!");
 		return 0;
 	} 
 	
 	for(int i = 0; i < quantidadeAlunos; i++){
-		printf("Digite a nota do aluno nº%i: ", i+1);
+		printf("Digite a nota do aluno nÂº%i: ", i+1);
 		scanf("%d", &reais[i]);
 		fflush(stdin);		
 	}
