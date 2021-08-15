7d101012100d83f17be88664fb6d2f3d396740ef
Raquel Santos <80298086+raquelsantoss@users.noreply.github.com>
2021-08-15 20:04:53 -0300

    Update ex10.cpp

--- lista3/ex10.cpp
+++ lista3/ex10.cpp
@@ -9,7 +9,7 @@ int main(){
 	int numerosLidos = 0, numeroAtual = 0;
 	
 	for(int i = 1; i <= 100; i++) {
-		printf("Digite o %d� n�mero: ", i);
+		printf("Digite o %dº número: ", i);
         scanf("%d", &numeroAtual);
         fflush(stdin);
         if(numeroAtual < 0){
@@ -21,7 +21,7 @@ int main(){
 	
 	system("cls");
 	
-	printf("\n\nA quantidade total de n�meros lidos foram: %d\n\n", numerosLidos);
+	printf("\n\nA quantidade total de números lidos foram: %d\n\n", numerosLidos);
 
 	return 0;
 }
