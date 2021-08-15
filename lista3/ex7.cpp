2993497921de64beeba4092e818bec7b01f10c3d
Raquel Santos <80298086+raquelsantoss@users.noreply.github.com>
2021-08-15 20:03:44 -0300

    Update ex7.cpp

--- lista3/ex7.cpp
+++ lista3/ex7.cpp
@@ -7,34 +7,34 @@ int main(){
 
 	int opcao, quantidadeAbacaxi = 0, quantidadeMaca = 0, quantidadePera = 0, quantidadeScanf = 0;
 	
-	printf("\nOl�, seja bem-vindo � Feira da Ceil�ndia !\n");
+	printf("\nOlá, seja bem-vindo à Feira da Ceilândia !\n");
 	while(opcao != 4){
-		printf("\n[1] Abacaxi \n[2] Ma�� \n[3] Pera \n[4] Finalizar pedido");
-		printf("\n\nSelecione qual fruta voc� deseja colocar no carrinho: ");
+		printf("\n[1] Abacaxi \n[2] Maçã \n[3] Pera \n[4] Finalizar pedido");
+		printf("\n\nSelecione qual fruta você deseja colocar no carrinho: ");
 		scanf("%d", &opcao);
 		
 		switch(opcao){
 			case 1:
-				printf("\nQuantos abacaxis voc� deseja comprar? ");
+				printf("\nQuantos abacaxis você deseja comprar? ");
 				scanf("%d", &quantidadeScanf);
 				quantidadeAbacaxi += quantidadeScanf;	
 			break;
 			case 2:
-				printf("\nQuantos ma��s voc� deseja comprar? ");
+				printf("\nQuantos maçãs você deseja comprar? ");
 				scanf("%d", &quantidadeScanf);
 				quantidadeMaca += quantidadeScanf;
 			break;
 			case 3:
-				printf("\nQuantos peras voc� deseja comprar? ");
+				printf("\nQuantos peras você deseja comprar? ");
 				scanf("%d", &quantidadeScanf);
 				quantidadePera += quantidadeScanf;	
 			break;
 			case 4:
 				printf("\nPedido finalizado! \n");
-				printf("Voc� comprou %d abacaxis, %d ma��s e %d peras", quantidadeAbacaxi, quantidadeMaca, quantidadePera);
+				printf("Você comprou %d abacaxis, %d maçãs e %d peras", quantidadeAbacaxi, quantidadeMaca, quantidadePera);
 			break;
 			default:
-				printf("\nOp��o n�o cadastrada, selecione uma dispon�vel!\n");
+				printf("\nOpção não cadastrada, selecione uma disponível!\n");
 		}
 	}
 	return 0;
