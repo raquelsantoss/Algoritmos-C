581589543ee1b7f9fa872cea9e4b4c0a5e7b0a5e
Raquel Santos <80298086+raquelsantoss@users.noreply.github.com>
2021-08-15 20:25:27 -0300

    Update ex05.cpp

--- lista6/ex05.cpp
+++ lista6/ex05.cpp
@@ -1,6 +1,4 @@
-// Nome: Raquel dos Santos Rezende
-// Matricula: UC21100930 
-// Curso: Ciência da Computação
+
 #include<stdio.h>
 #include<locale.h>
 #include<stdlib.h>
@@ -21,10 +19,10 @@ int main(){
 		
 		
 		do{
-			printf("Quantas crianças  irão ser analisados na pesquisa: ");
+			printf("Quantas crianÃ§as  irÃ£o ser analisados na pesquisa: ");
 			scanf("%d", &criancasTotal);
 			if(criancasTotal<=0){
-				printf("\nDigite um valor válido!\n\n ");		
+				printf("\nDigite um valor vÃ¡lido!\n\n ");		
 			}
 		}while(criancasTotal<=0);
 		
@@ -34,7 +32,7 @@ int main(){
 		do{
 			
 			
-			printf("\n\nDigite o sexo da %dª criança [F/M]. (Digite C para prosseguir): ", contador);
+			printf("\n\nDigite o sexo da %dÂª crianÃ§a [F/M]. (Digite C para prosseguir): ", contador);
 			
 			do{
 				scanf("%c", &sexoCrianca);
@@ -44,10 +42,10 @@ int main(){
 			
 			
 			do{
-				printf("\n\nDigite quantos meses de vida tem a %dª criança: ", contador);
+				printf("\n\nDigite quantos meses de vida tem a %dÂª crianÃ§a: ", contador);
 				scanf("%d", &mesesVida);
 				if(mesesVida<=0){
-					printf("\nDigite um valor válido!");
+					printf("\nDigite um valor vÃ¡lido!");
 				}
 			}while(mesesVida<=0);
 			
@@ -80,11 +78,11 @@ int main(){
 		
 	
 	printf("Resultados da Pesquisa:\n\n");
-	printf("Total de Crianças: %d\n", criancasTotal);
-	printf("Total de Crianças Mortas: %d\n", criancasMortas);
+	printf("Total de CrianÃ§as: %d\n", criancasTotal);
+	printf("Total de CrianÃ§as Mortas: %d\n", criancasMortas);
 	printf("Taxa de Mortalidade Infantil: %d %%\n", porcentagemMortos);
 	printf("Taxa de Meninos Mortos: %d %%\n", porcentagemMortosMasculino);
-	printf("Crianças que morreram com menos de 24 meses de vida: %d %%", porcentagemPrematuros);
+	printf("CrianÃ§as que morreram com menos de 24 meses de vida: %d %%", porcentagemPrematuros);
 		
 	
  	return 0;
